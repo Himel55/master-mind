749a1577bc256c1f668c9bbd2914f23f9e68aee5
himel <himel55@gmail.com>
2024-11-24 18:49:47 +1100

    Add circles

--- src/gui_app.c
+++ src/gui_app.c
@@ -7,40 +7,46 @@
 #include <SDL2/SDL.h>
 
 // Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 1600;
+const int SCREEN_HEIGHT = 900;
 
-int gui_main(void)
-{
-  // The window we'll be rendering to
+#define SDL_COLOUR(colour) (((colour) >> 3 * 8) & 0xFF), (((colour) >> 2 * 8) & 0xFF), (((colour) >> 1 * 8) & 0xFF), (((colour) >> 0 * 8) & 0xFF)
+#define BACKGROUND_COLOUR 0x1E1E1EFF
+#define FOREGROUND_COLOUR 0xFFFFFFFF
+#define LARGE_CIRCLE_RADIUS 30
+#define LARGE_CIRCLE_THICKNESS 5
+#define SMALL_CIRCLE_RADIUS 15
+#define SMALL_CIRCLE_THICKNESS 2
+#define PADDING 20
+#define SMALL_PADDING 5
+
+static void draw_empty_circle(SDL_Renderer *renderer, int x, int y, int radius, int thickness) {
+  SDL_SetRenderDrawColor(renderer, SDL_COLOUR(FOREGROUND_COLOUR));
+  for (int h = 0; h < 2 * radius; h++) {
+    for (int w = 0; w < 2 * radius; w++) {
+      int dy = h - radius;
+      int dx = w - radius;
+      if ((dx*dx + dy*dy < radius*radius) && 
+          (dx*dx + dy*dy >= (radius - thickness)*(radius - thickness))) {
+        SDL_RenderDrawPoint(renderer, x + dx, y + dy);
+      }
+    }
+  }
+}
+
+int gui_main(void) {
   SDL_Window *window = NULL;
+  SDL_Renderer *renderer = NULL;
 
-  // The surface contained by the window
-  SDL_Surface *screenSurface = NULL;
-
-  // Initialize SDL
-  if (SDL_Init(SDL_INIT_VIDEO) < 0)
-  {
-    printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    fprintf(stderr, "SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
     return EXIT_FAILURE;
   }
 
-  // Create window
-  window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-  if (window == NULL)
-  {
-    printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+  if (SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN, &window, &renderer) < 0) {
+    fprintf(stderr, "window could not be created! SDL_Error: %s\n", SDL_GetError());
     return EXIT_FAILURE;
   }
-
-  // Get window surface
-  screenSurface = SDL_GetWindowSurface(window);
-
-  // Fill the surface white
-  SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-
-  // Update the surface
-  SDL_UpdateWindowSurface(window);
 
   // Hack to get window to stay up
   SDL_Event e;
@@ -52,6 +58,35 @@ int gui_main(void)
       if (e.type == SDL_QUIT)
         quit = true;
     }
+
+    if (SDL_SetRenderDrawColor(renderer, SDL_COLOUR(BACKGROUND_COLOUR)) < 0) {
+      fprintf(stderr, "Render draw colour error! SDL_Error: %s\n", SDL_GetError());
+    }
+
+    if (SDL_RenderClear(renderer) < 0) {
+      fprintf(stderr, "Render clear error! SDL_Error: %s\n", SDL_GetError());
+    }
+
+  // TODO magic numbers
+  int circles_offset_width = SCREEN_WIDTH / 2 - ((5 * PADDING + 4 * (2 * LARGE_CIRCLE_RADIUS)) / 2);
+  int circles_offset_height = SCREEN_HEIGHT / 2 - ((9 * PADDING + 10 * (2 * LARGE_CIRCLE_RADIUS)) / 2);
+  for (int j = 0; j < 10; j++) {
+    for (int i = 0; i < 4; i++) {
+      draw_empty_circle(renderer, (i * (2 * LARGE_CIRCLE_RADIUS + PADDING) + LARGE_CIRCLE_RADIUS + circles_offset_width), 
+                        (j * (2 * LARGE_CIRCLE_RADIUS + PADDING) + LARGE_CIRCLE_RADIUS + circles_offset_height), LARGE_CIRCLE_RADIUS, LARGE_CIRCLE_THICKNESS);
+    }
+
+    int mini_circles_offset_width = 4 * (2 * LARGE_CIRCLE_RADIUS + PADDING) + LARGE_CIRCLE_RADIUS + PADDING + circles_offset_width;
+    int mini_circles_offset_height = (j * (2 * LARGE_CIRCLE_RADIUS + PADDING) + SMALL_CIRCLE_RADIUS + circles_offset_height);
+    for (int i = 0; i < 2; i++) {
+      for (int k = 0; k < 2; k++) {
+        draw_empty_circle(renderer, i * (2 * SMALL_CIRCLE_RADIUS + SMALL_PADDING) + mini_circles_offset_width, 
+                          k * (2 * SMALL_CIRCLE_RADIUS + SMALL_PADDING) + mini_circles_offset_height, SMALL_CIRCLE_RADIUS, SMALL_CIRCLE_THICKNESS);
+      }
+    }
+
+  }
+    SDL_RenderPresent(renderer);
   }
 
   // Destroy window
